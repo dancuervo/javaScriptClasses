7ed375bc4f997f0cd25d7b26eeee4e7aeac5558e
Jose Daniel Cenoz Santos <josedanielc@google.com>
2019-05-22 12:02:25 -0300

    otro commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//Ejercicios con clases
+
+    
+    class User{
+        constructor(name, surname, age){
+            this._name       = name;
+            this._surname    = surname;
+            this._age        = age;
+        }
+    
+       setName(name){
+            this._name = name;
+        }
+
+       getName(){
+           return this._name;
+       } 
+    }
+
+// fin clase User
+
